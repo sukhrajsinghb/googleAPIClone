6c5c5211214b7bd3d26aec338b268380b6ec4784
rwclutterbuck <rwclutterbuck@outlook.com>
2022-01-26 16:57:20 +0000

    search page structured

--- client/bundle.js
+++ client/bundle.js
@@ -4,11 +4,19 @@ const form = document.querySelector("form");
 form.addEventListener("submit", onClick);
 
 async function onClick(e) {
-  console.log(e);
+  // console.log(form[0].value);
   e.preventDefault();
+  localStorage.setItem("search", form[0].value);
   location.href = "/search.html";
+
   // search = document.querySelector("#search").value;
   //   await fetch(`127.0.0.1:3000/search/${search}`).then((resp) => resp.json());
+}
+
+if (document.title !== "Google") {
+  result = localStorage.getItem("search");
+  document.title = result;
+  document.querySelector("form")[0].value = result;
 }
 
 },{}]},{},[1]);
