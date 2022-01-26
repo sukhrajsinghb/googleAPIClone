f4501246d4f43f3e2ff07159dbc4d9003df48aa3
Sukhraj Bhandal <sukhrajbhandal1998@gmail.com>
2022-01-26 17:42:52 +0000

    new data added

--- server/app.js
+++ server/app.js
@@ -22,10 +22,10 @@ app.get('/search/:name', (req, res) => {
     try {
         let requestedSearch = req.params.name
         let matchingSearch = search.find((item) => item.title.toLowerCase() === requestedSearch.toLowerCase());
-        if(!matchingSearch) { throw new Error(`We don't have a page called ${requestedSearch}`)}
+        if(!matchingSearch) { throw new Error(`Sorry we don't have a page called ${requestedSearch}`)}
         res.json(matchingSearch)
     } catch (error) {
-        res.status(404).redirect(`https://en.wikipedia.org/w/index.php?search=${requestedSearch}&title=Special:Search&profile=advanced&fulltext=1&ns0=1`);
+        res.status(404).json({message: error.message});
     }
 })
 
